2a53b1e8e8a3cc9f31335f5f569884e7bd821803
Jake Butler <tjakebutler@gmail.com>
2020-11-17 11:56:20 -0600

    commit to merge

---
+++ src/scripts/Messages/MessageList.js
@@ -0,0 +1,5 @@
+// Given an active user sees a chat message from another user
+// And wants to make that user a friend
+// When the active user clicks on the other user's name in the chat history
+// Then a message should be presented to the active user asking for verification of adding the other user to their friend list
+// 
