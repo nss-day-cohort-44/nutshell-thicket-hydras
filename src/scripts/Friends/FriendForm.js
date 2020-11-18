9e4b90c6b792e1111b605fd94e6bfad6c75456ec
admin <christopherleorondeau@gmail.com>
2020-11-17 21:11:14 -0600

    made friend bar render after friend button click

--- src/scripts/Friends/FriendForm.js
+++ src/scripts/Friends/FriendForm.js
@@ -2,11 +2,37 @@ const contentTarget = document.querySelector(".friendsContainer")
 const eventHub = document.querySelector(".container")
 
 
-eventHub.addEventListener("userAuthenticated", () => FriendForm())
+eventHub.addEventListener("userAuthenticated", () => AddFriendButton())
+
+export const AddFriendButton = () => {
+    return  contentTarget.innerHTML =  `
+    
+    
+    <button id="saveFriend">Add a Friend</button>`
+}
 
 export const FriendForm = () => {
-    return  contentTarget.innerHTML =  `
-    <input id="friendsList--friendName" type="text" placeholder="Search For Names">
+    contentTarget.innerHTML +=`<input id="friendsList--friendName" type="text" placeholder="Search For Names"></input>`}
+
+eventHub.addEventListener("click" , event => { 
+
+    if (event.target.id === "saveFriend") {
+        FriendForm()
+    }
+
+ } 
+ )
+
+
+
+
+
+// eventHub.addEventListener("click", event => {
+
+//     if (event.target.id === "witnessSelect") {
+        
+//         const customEvent = new CustomEvent("witnessClicked")
     
-    <button id="saveFriend">Add Friend</button>`
-}
+//         eventHub.dispatchEvent(customEvent)
+//     }
+// })
--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,12 +1,12 @@
 import { EventsList } from "./Events/EventsList.js"
-import { FriendForm } from "./Friends/FriendForm.js"
+import { AddFriendButton, FriendForm } from "./Friends/FriendForm.js"
 import { renderWitnessButton } from "./Events/DisplayNewEvents.js"
 
 
 
 export const Nutshell = () => {
     EventsList()
-    FriendForm()
+    AddFriendButton()
     renderWitnessButton()
 
     // Render all your UI components here
